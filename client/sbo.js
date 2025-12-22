cb0378f7d52872411f40f6e802da0256136a78ed
Bacon Ate Her <bacon@ateher.com>
2025-12-21 21:04:39 -0800

    start linking buttons to functions

--- client/sbo.js
+++ client/sbo.js
@@ -1,31 +1,35 @@
 class Scoreboard {
 	constructor() {
-		console.log("constructing the scoreboard");
-
-		if (!localStorage.getItem("currentGame")) {
+		//if (!localStorage.getItem("currentGame")) {
 			this.current = {};
 			this.current.jam = 0;
-			localStorage.setItem("currentGame", this.current);
-		} else {
-			this.current = localStorage.getItem("currentGame");
-		}
+			//localStorage.setItem("currentGame", this.current);
+		//} else {
+			//this.current = localStorage.getItem("currentGame");
+		//}
 
-		console.log(this);
+		this.start_jam = this.start_jam.bind(this);
+		document.querySelector('#start-jam').addEventListener('click', this.start_jam);
+
+		this.set_trip_points = this.set_trip_points.bind(this);
+		let trip_points = document.querySelectorAll(".trip-points");
+		for (let i = 0; i < trip_points.length; i++) {
+			trip_points[i].addEventListener('click', this.set_trip_points);
+		}
 	}
 
 	get_trip_points(team) {
 	}
 
-	set_trip_points(team, pts) {
+	set_trip_points(e) {
+		console.log(e.target.getAttribute("team"));
 	}
 
-	start_jam() {
-		console.log(this);
+	start_jam(e) {
 		this.current.jam++;
-		console.log("Starting jam " + this.current.jam);
-		let jams = document.querySelector(".jam");
-		for (j in jams) {
-			j.innerContent = this.current.jam;
+		let jams = document.querySelectorAll(".jam");
+		for (let i = 0; i < jams.length; i++) {
+			jams[i].innerText = this.current.jam;
 		}
 	}
 }
@@ -33,8 +37,3 @@ class Scoreboard {
 let scoreboard = new Scoreboard();
 
 export { Scoreboard };
-
-export function start_jam() {
-	debugger;
-	scoreboard.start_jam();
-}
